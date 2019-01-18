13db87fa2a90c97869208bbab446d618315149b0
Le-Yo <koskeyhoward@gmail.com>
2019-01-18 10:13:27 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="UTF-8" />
+<meta name="description" content="Free jQuery and CSS3 based analog clock widget. Codehim Clock Plugin helps you to create an awesome real looking analog clock in your website app project with date and days." />
+<title> civian</title>
+
+   <!--jQuery-->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+   <!--Codehim Clock JS-->
+<script src="js/jquery.codehim.clock.js"></script>
+
+   <!--Codehim Clock CSS-->
+<link rel="stylesheet" href="css/codehim-clock.css" />
+
+<script>
+$(document).ready(function(){ 
+
+$(".clock-place").CodehimClock({
+  
+ });
+
+}); 
+</script>
+
+</head>
+<body>
+
+<div class="clock-place"> </div>
+
+  
+
+</body>
+</html>
